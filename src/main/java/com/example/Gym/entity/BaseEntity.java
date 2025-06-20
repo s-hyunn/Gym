6de15480fea6d154b58eb3ac5cd81aca20ae4ebf
chryu98 <chboy98@daum.net>
2025-06-20 12:47:04 +0900

    문의 등록시 qno때문에 무결성 제약 뜨는거 해결하고, 사용자가 필드 선택해서 글 등록하는 기능 추가

--- src/main/java/com/example/Gym/entity/BaseEntity.java
+++ src/main/java/com/example/Gym/entity/BaseEntity.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
@@ -12,6 +13,7 @@ import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@EnableJpaAuditing 
 public abstract class BaseEntity {
 
     @CreatedDate
--- src/main/java/com/example/Gym/entity/QnaEntity.java
+++ src/main/java/com/example/Gym/entity/QnaEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -15,9 +16,14 @@ import lombok.Data;
 @Data
 public class QnaEntity extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long qno;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "qna_seq_gen")
+	@SequenceGenerator(
+	    name = "qna_seq_gen",           // JPA 식별용 이름
+	    sequenceName = "QNA_SEQ",       // 오라클 실제 시퀀스 이름 (대소문자 주의)
+	    allocationSize = 1              // 1씩 증가
+	)
+	private Long qno;
 
     @ManyToOne
     @JoinColumn(name = "userid")
