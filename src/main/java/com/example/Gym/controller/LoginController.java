420a86f081fb303d0787b3e7dae7326a0e0fd5d9
Yamtan1 <gndndk12305@naver.com>
2025-06-20 14:05:21 +0900

    매핑수정
    1231414

--- src/main/java/com/example/Gym/controller/LoginController.java
+++ src/main/java/com/example/Gym/controller/LoginController.java
@@ -1,15 +1,18 @@
 package com.example.Gym.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.example.Gym.entity.UserEntity;
 import com.example.Gym.enums.Role;
 import com.example.Gym.service.UserService;
+
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -34,21 +37,14 @@ public class LoginController {
             if (user.getRole() == Role.ROLE_MANAGER) {
                 return "redirect:/qna/manager/view";
             } else {
-                return "redirect:/"; // 일반 회원 메인 페이지
+                return "redirect:/myPage"; // 일반 회원 메인 페이지
             }
         } else {
             model.addAttribute("error", "아이디 또는 비밀번호가 잘못되었습니다.");
             return "login"; // 다시 로그인 페이지로
         }
-        else {
-        	return "redirect:/login";
-        }
     }
     
-    @GetMapping("/manager")
-    public String manager() {
-    	return "manager";
-    }
     @GetMapping("/myPage")
     public String myPage() {
     	return "myPage";
--- src/main/java/com/example/Gym/controller/QnaController.java
+++ src/main/java/com/example/Gym/controller/QnaController.java
@@ -5,14 +5,17 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.Gym.entity.QnaEntity;
 import com.example.Gym.entity.UserEntity;
+import com.example.Gym.service.QnaService;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
@@ -45,10 +48,10 @@ public class QnaController {
 
     @PostMapping("/qna/answer")
     @ResponseBody
-    public ResponseEntity<?> answerQna(@RequestParam("qno") Long qno,
+    public String answerQna(@RequestParam("qno") Long qno,
                                        @RequestParam("answer") String answer) {
         qnaService.answer(qno, answer);
-        return "redirect:/manager";
+        return "redirect:/manager_qna";
     }
 
     @GetMapping("/qna/manager/view")
